940dfa79d5dfaa23f6683b88158b2bfb340b9997
anishah78 <anishahhussain@hotmail.com>
2024-01-16 19:29:44 +0000

    defining networkign services with Iac, initialised terraformm input variables

--- aks-terraform/networking-module/main.tf
+++ aks-terraform/networking-module/main.tf
@@ -1,3 +1,48 @@
+terraform {
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "3.0.0" 
+        }
+    }
+}
+# defined azure provider 
+
+provider "azurerm" {
+    features {}
+  client_id       = "66cd9882-ab20-47ac-bd58-e077cdcd37c0"
+  client_secret   = "_aW8Q~YtSrXXk5RZK2E-vBrkKmFuFVbvmXFN.bHn"
+  subscription_id = "69974029-8ad2-4473-b2e5-a1fb53a6ad13"
+  tenant_id       = "47d4542c-f112-47f4-92c7-a838d8a5e8ef"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "my-resource-group"
+  location = "UK South"
+}
+# Declared azure storage account 
+
+
+resource "azurerm_virtual_network" "my_vnet" {
+  name           = "my_devops_vnet"
+  address_space  = var.vnet_address_space
+  resource_group_name  = azurerm_resource_group.example
+  depends_on = [azurerm_resource_group.example]
+} 
+
+# Defining the Azure Storage Account resource
+
+resource "azurerm_storage_account" "example" {
+  name                     = "azurerm_storage_account"
+  resource_group_name      = "my-resource-group"
+  location                 = "UK South"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  depends_on = [azurerm_resource_group.example]
+}
+
+# Essential networking resources for an AKS cluster
+
 resource "azurerm_resource_group" "networking" {
   name     = var.resource_group_name
   location = var.location
@@ -21,7 +66,7 @@ resource "azurerm_subnet" "worker_node_subnet" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 resource "azurerm_network_security_group" "aks_nsg" {
-  name                = "azurerm_security_group"
+  name                = "aks_security_group"
   location            = azurerm_resource_group.networking.location
   resource_group_name = azurerm_resource_group.networking.name
 }
@@ -50,4 +95,4 @@ resource "azurerm_network_security_rule" "ssh" {
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.networking.name
   network_security_group_name = azurerm_network_security_group.aks_nsg.name
-}
+  }
--- aks-terraform/networking-module/variables.tf
+++ aks-terraform/networking-module/variables.tf
@@ -10,9 +10,14 @@ variable "location" {
   default     = "location"  # Provided a default value
 }
 
-variable "vnet_address_space" {
-  type        = list(string)
-  description = "Address space for the Virtual Network (VNet)."
-  default     = ["82.31.245.149"]  # Provided a default value for the source address
+variable "instance_type" {
+  description = "The type of EC2 instance to create."
+  type        = string
+  default     = "t2.micro"
 }
 
+variable "my_IP" {
+  type        = string
+  description = "Address space for the Virtual Network (VNet)."
+  default     = "82.31.245.149"  # Provided a default value for the source address
+}
